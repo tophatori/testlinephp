4ddd07ed3f8f5705075a2ae5a87a8559b5c55bb1
tophatori <likhittop@gmail.com>
2019-08-04 17:08:29 +0700

    Add files via upload

---
+++ line_rss.php
@@ -0,0 +1,51 @@
+<?php
+
+/////////////////// ดึงข่าว ////////////////
+$ch = curl_init('http://fetchrss.com/rss/5d469f4d8a93f89a718b45685d46a0a38a93f8157e8b4567.xml');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+$contents = curl_exec($ch);
+curl_close($ch);
+ 
+$xml = new SimpleXmlElement($contents);
+//for($i=0; $i<count($xml->channel->item); $i++){
+	for($i=0; $i<5; $i++){
+$url = $xml->channel->item[$i]->link;
+$title = $xml->channel->item[$i]->title;
+$description = $xml->channel->item[$i]->description;
+$news .= $title."\n".$url."\n\n";
+
+}
+/////////////////////////////////////
+//token 
+$token_line = "aZwx0YSUCbOhljuJpTJAng2FUn2VaEH72QKBr6JZZzU";
+
+$message = $_REQUEST['message'];
+$chOne = curl_init(); 
+curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+// SSL USE 
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
+//POST 
+curl_setopt( $chOne, CURLOPT_POST, 1); 
+// Message 
+curl_setopt( $chOne, CURLOPT_POSTFIELDS, $message); 
+//ถ้าต้องการใส่รุป ให้ใส่ 2 parameter imageThumbnail และimageFullsize
+curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$news"); 
+// follow redirects 
+curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
+//ADD header array 
+$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$token_line.'', ); 
+curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
+//RETURN 
+curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
+$result = curl_exec( $chOne ); 
+//Check error 
+if(curl_error($chOne)) { echo 'error:' . curl_error($chOne); } 
+else { $result_ = json_decode($result, true); 
+echo "status : ".$result_['status']; echo "message : ". $result_['message']; } 
+//Close connect 
+curl_close( $chOne ); 
+
+
+?>
